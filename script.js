cccc752ad9bd32bee5cd8339adb7897432bcfccb
codessbabe <oana.anghelachi@gmail.com>
2021-03-08 02:40:04 +0100

    Set dynamic year

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const hours = document.getElementById('hours')
 const minutes = document.getElementById('minutes')
 const seconds = document.getElementById('seconds')
 const countdown = document.getElementById('countdown')
+const year = document.getElementById('year')
+
 
 const currentYear = new Date().getFullYear();
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`)
 const devInterviewTime = new Date(`March 11 ${currentYear} 00:00:00`)
+
+// Set background year
+year.innerText = currentYear + 1;
 
 function updateCountdown() { 
   const currentTime = new Date(); 
